f84f73676757e854af3fd799c945f923c7a641c1
Harrison Jude <harrison@hjude.com>
2025-01-13 16:35:55 -0500

    updated render.php to match edit

--- build/entry/render.php
+++ build/entry/render.php
@@ -2,7 +2,17 @@
 /**
  * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
  */
+
+$mediaURL = "url(" . $attributes['mediaURL'] . ")";
+
 ?>
 <div <?php echo get_block_wrapper_attributes(); ?>>
-	<?php echo $content ?>
+	<span class="displayYear displayCentury"><?php echo $attributes['displayCentury'] ?></span>
+	<span class="displayYear displayDecade"><?php echo $attributes['displayDecade'] ?></span>
+
+	<div class="entryCover" style="background-image:<?php echo $mediaURL ?>;"></div>
+
+	<div class="entryContent">
+		<?php echo $content ?>
+	</div>
 </div>
