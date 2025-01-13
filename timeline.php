7c063d6e21c5a22f33b0676aa149fa5c85f76788
Harrison Jude <harrison@hjude.com>
2025-01-13 16:16:59 -0500

    changed version to 0.1.2

--- timeline.php
+++ timeline.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       Timeline
  * Description:       Example block scaffolded with Create Block tool.
- * Version:           0.1.0
+ * Version:           0.1.2
  * Requires at least: 6.7
  * Requires PHP:      7.4
  * Author:            The WordPress Contributors
